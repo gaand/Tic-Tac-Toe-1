343166be1b4f77d3bd28492e19a04b99084b2603
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-01-31 22:17:09 -0500

    small changes

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -4,7 +4,19 @@ let changeColorRed = function () {
   $(this).css('background-color', 'red');
 };
 
+let reset = function () {
+  $('.square').css('background-color', '#4e4e4e');
+};
+
+
 $('.square').on('click', changeColorRed);
+
+$('button').on('click', reset);
+
+
+
+
+
 
 
 
