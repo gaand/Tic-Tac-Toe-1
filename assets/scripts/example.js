5f591bc00d619e42910caa3d706eef09f6437e88
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-02-01 18:49:52 -0500

    adds turn validation

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,12 +1,24 @@
 'use strict';
 
 
+//const board = $('.square');
+
+
+
+
 let resetGame = function () {
   $('.square').text('');
 };
 
+$('button').on('click', resetGame);
 let turnCount = 0;
+
+//isMoveValid() -- check to see if space is occupied by X or O.
+
+
+
 let makeMark = function () {
+  if ($(this).text() === ''){
   if (turnCount % 2 === 0) {
     $(this).text('X');
     //checkfor win
@@ -15,11 +27,14 @@ let makeMark = function () {
     //checkfor win
   }
   turnCount++;
+  }
 };
+
 
 $('.square').on('click', makeMark);
 
-$('button').on('click', resetGame);
+
+
 
 
 
