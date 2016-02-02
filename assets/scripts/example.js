640604f652f21fdefd922c5b490fe58667c47e90
nwilcox11 <wilcox.nicholas.m@gmail.com>
2016-02-02 15:36:24 -0500

    initial game logic

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -7,31 +7,73 @@
 
 
 let resetGame = function () {
-  $('.square').text('');
+  $('.square').html('');
 };
 
 $('button').on('click', resetGame);
+
+
+
+//let player = ['X', 'O'];
+const board = ['','','','','','','','',''];
 let turnCount = 0;
 
-//isMoveValid() -- check to see if space is occupied by X or O.
+let checkRow = function(a, b, c) {
+    if (a === 'X' && b === 'X' && c === 'X') {
+        alert('X wins');
+        return 'X is the winner';
+    } else if (a === 'O' && b === 'O' && c === 'O') {
+      alert('O wins');
+        return 'O is the winner';
+
+    }
+};
+
+let getWinner = function () {
+    // checking rows
+    checkRow(board[0], board[1], board[2]);
+    checkRow(board[3], board[4], board[5]);
+    checkRow(board[6], board[7], board[8]);
+    // checking columns
+    checkRow(board[0], board[3], board[6]);
+    checkRow(board[1], board[4], board[7]);
+    checkRow(board[2], board[5], board[8]);
+    // checking diagonals
+    checkRow(board[0], board[4], board[8]);
+    checkRow(board[2], board[4], board[6]);
+};
 
 
-
-let makeMark = function () {
-  if ($(this).text() === ''){
-  if (turnCount % 2 === 0) {
-    $(this).text('X');
-    //checkfor win
+  let makeMark = function () {
+    if ($(this).html() === ''){
+      if (turnCount % 2 === 0) {
+      $(this).html('X');
+      board[event.target.id] = 'X';
+      //alert(event.target.id);
+      //alert(board);
+      getWinner();
   } else {
-    $(this).text('O');
-    //checkfor win
+    $(this).html('O');
+    board[event.target.id] = 'O';
+      //alert(event.target.id);
+      //alert(board);
+    getWinner();
   }
   turnCount++;
   }
 };
+  $('.square').on('click', makeMark);
 
 
-$('.square').on('click', makeMark);
+
+
+
+
+
+
+
+
+
 
 
 
